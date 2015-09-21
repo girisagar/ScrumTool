49c4b1ecb5d4a9fc7681da7f9b54170b072b7ff4
Bibek Chitrakar <bibek.chitrakar@gmail.com>
2015-09-21 17:10:49 -0500

    app report added

---
+++ report/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+# from report.views import SprintBurndownChartView
+
+# urlpatterns = [
+#     url(
+#         # r'sprint/(?P<sprint_id>\d+)/$',
+#         r'$',
+#         SprintBurndownChartView.as_view(),
+#         name = "report_sprint"
+#     ),
+    
+# ]
