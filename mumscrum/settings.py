d16c62330ab4e71d0bd3fe8aeae26bbdab41e8f4
Sagar Giri <sagar.giri95@gmail.com>
2015-09-23 09:29:31 -0500

    small fixes

--- mumscrum/settings.py
+++ mumscrum/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'mumscrum.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'MumScrumDB.sqlite3'),
     }
 }
 
